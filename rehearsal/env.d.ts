54a92f87bfbf32d44f131f86a00780f30e0bc77b
mohamad <mohamad>
2025-10-04 19:30:02 +0200

    Remove rehearsal, we pivot

--- rehearsal/env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-
-// Minimal Cloudflare Pages function type to satisfy TypeScript
-declare type PagesFunction<Env = unknown> = (context: {
-  request: Request
-  env: Env
-  ctx: any
-}) => Promise<Response>
